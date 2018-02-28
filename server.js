b0c7f255f72e08f8e170f02e438efda6d15d2846
yang <tmp@tmpdeMacBook-Pro.local>
2018-02-28 20:38:16 +0800

    release v1

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const isDev = process.env.NODE_ENV !== 'production';
+const fs = require('fs');
+const path = require('path');
 const mongoose = require('mongoose');
 const app = new (require('koa'))();
 const config = require(`./config${isDev ? '.dev' : ''}`);
@@ -11,6 +13,10 @@ app.use(async (ctx, next) => {
     ctx.config = config;
     return next(ctx);
 });
+// 登录状态监测插件
+if (fs.existsSync(path.join(__dirname, './login.js'))) {
+    app.use(require('./login'));
+}
 require('./server/index').init(app, config);
 if (isDev) {
     require('./dev').init(app, config);
