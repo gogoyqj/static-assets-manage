99e2dab42ea76a0ede91cf4c12f1e14a1cffc9b1
yang <tmp@tmpdeMacBook-Pro.local>
2018-03-01 10:59:55 +0800

    fix build bug

--- build.js
+++ build.js
@@ -19,7 +19,7 @@ compiler.run((err) => {
   const html = fs.readFileSync(path.join(__dirname, 'static', 'index.html'), encoding);
   const ver = Date.now();
   fs.writeFileSync(path.join(outputPath, '..', 'index.html'), html
-    .replace(/stm-main.js/g, `bundle.js?ver=${ver}`)
-    .replace(/stm-main.css/g, `main.css?ver=${ver}`), encoding);
+    .replace(/stm-main.js/g, `stm-main.js?ver=${ver}`)
+    .replace(/stm-main.css/g, `stm-main.css?ver=${ver}`), encoding);
   console.log('build success');
 });
